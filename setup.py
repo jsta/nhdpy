efd4018052fd8819d5762cb7bb958e9ae7aa601e
jsta <stachel2@msu.edu>
2020-11-13 09:57:14 -0500

    specify markdown readme in setup

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n',
+    long_description_content_type="text/markdown",
     include_package_data=True,
     keywords='nhdpy',
     name='nhdpy',
